a7fc977c108f936c7314edd4849ddd845fca2375
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2023-11-23 17:17:02 +0300

    Updated adWindow class

--- GameOfLife/AdWindow.cs
+++ GameOfLife/AdWindow.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -12,11 +14,12 @@ namespace GameOfLife
         private readonly DispatcherTimer adTimer;
         private int imgNmb;     // the number of the image currently shown
         private string link;    // the URL where the currently shown ad leads to
-        
-    
+        private readonly Dictionary<int, ImageBrush> adImages;
+        private static readonly Random rnd = new Random();
+
+
         public AdWindow(Window owner)
         {
-            Random rnd = new Random();
             Owner = owner;
             Width = 350;
             Height = 100;
@@ -26,7 +29,14 @@ namespace GameOfLife
             Cursor = Cursors.Hand;
             ShowActivated = false;
             MouseDown += OnClick;
-            
+
+            adImages = new Dictionary<int, ImageBrush>
+            {
+                {1, new ImageBrush { ImageSource = new BitmapImage(new Uri("ad1.jpg", UriKind.Relative))}},
+                {2, new ImageBrush { ImageSource = new BitmapImage(new Uri("ad2.jpg", UriKind.Relative))}},
+                {3, new ImageBrush { ImageSource = new BitmapImage(new Uri("ad3.jpg", UriKind.Relative))}}
+            };
+
             imgNmb = rnd.Next(1, 3);
             ChangeAds(this, new EventArgs());
 
@@ -41,15 +51,27 @@ namespace GameOfLife
 
         private void OnClick(object sender, MouseButtonEventArgs e)
         {
-            System.Diagnostics.Process.Start(link);
+            OpenUrl(link);
             Close();
         }
-        
+
+        public static void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
         protected override void OnClosed(EventArgs e)
         {
             Unsubscribe();
             base.OnClosed(e);
-        } 
+        }
 
         public void Unsubscribe()
         {
@@ -58,34 +80,30 @@ namespace GameOfLife
 
         private void ChangeAds(object sender, EventArgs eventArgs)
         {
-            
+
             ImageBrush myBrush = new ImageBrush();
-            
+
             switch (imgNmb)
             {
                 case 1:
-                    myBrush.ImageSource =
-                        new BitmapImage(new Uri("ad1.jpg", UriKind.Relative));
+                    Background = adImages[1];
                     Background = myBrush;
                     link = "http://example.com";
                     imgNmb++;
                     break;
                 case 2:
-                    myBrush.ImageSource =
-                        new BitmapImage(new Uri("ad2.jpg", UriKind.Relative));
+                    Background = adImages[2];
                     Background = myBrush;
                     link = "http://example.com";
                     imgNmb++;
                     break;
                 case 3:
-                    myBrush.ImageSource =
-                        new BitmapImage(new Uri("ad3.jpg", UriKind.Relative));
+                    Background = adImages[3];
                     Background = myBrush;
                     link = "http://example.com";
                     imgNmb = 1;
                     break;
             }
-            
         }
     }
 }
